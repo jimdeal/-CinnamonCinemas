e121cf016807e83d96dbdf0f58679255e4821c8f
jimdeal <jimdeal@hotmail.com>
2023-02-22 13:10:16 +0000

    add in first test - simple buy ticket : build fail

---
+++ CinnamonCinemas/src/main/java/CinamonCinema/CinnamonCinema.java
@@ -0,0 +1,9 @@
+package CinamonCinema;
+
+public class CinnamonCinema {
+
+    public boolean buyFilmTickets(String user, int numberOfTickets){
+        return false;
+    }
+
+}
---
+++ CinnamonCinemas/src/test/java/CinamonCinema/CinnamonCinemaTest.java
@@ -0,0 +1,10 @@
+package CinnamonCinema;
+
+import org.junit.jupiter.api.Test;
+
+class CinnamonCinemaTest {
+
+    @Test
+    void buyFilmTickets() {
+    }
+}
