32088d1e66013bac0dfbb53ca2039a3b912d74b4
jimdeal <jimdeal@hotmail.com>
2023-02-22 13:38:39 +0000

    first - simple seat buy unit test

--- CinnamonCinemas/src/main/java/CinamonCinema/CinnamonCinema.java
+++ CinnamonCinemas/src/main/java/CinamonCinema/CinnamonCinema.java
@@ -2,8 +2,26 @@ package CinamonCinema;
 
 public class CinnamonCinema {
 
+    private final int NUMBER_OF_ROWS = 3;
+    private final int NUMBER_OF_SEATS_IN_ROW = 56;
+
+    private String[][] screenSeats = new String[][]{{"-1","-1","-1","-1","-1","0"},
+            {"-1","-1","-1","-1","-1","0"},{"-1","-1","-1","-1","-1","0"}};
+
     public boolean buyFilmTickets(String user, int numberOfTickets){
-        return false;
+        boolean purchaseSuccess = false;
+        for(int row = 0; row < NUMBER_OF_ROWS; row++){
+            int takenSeatsInRow = Integer.parseInt(screenSeats[row][5]);
+            if(NUMBER_OF_SEATS_IN_ROW - takenSeatsInRow - 1 >= numberOfTickets){
+                for(int fillSeats = takenSeatsInRow; fillSeats < numberOfTickets; fillSeats++){
+                    screenSeats[row][fillSeats] = user;
+                }
+                screenSeats[row][5] = Integer.toString(numberOfTickets);
+                purchaseSuccess = true;
+            }
+        }
+
+        return purchaseSuccess;
     }
 
 }
--- CinnamonCinemas/src/test/java/CinamonCinema/CinnamonCinemaTest.java
+++ CinnamonCinemas/src/test/java/CinamonCinema/CinnamonCinemaTest.java
@@ -1,10 +1,15 @@
-package CinnamonCinema;
+package CinamonCinema;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CinnamonCinemaTest {
 
     @Test
     void buyFilmTickets() {
+        CinnamonCinema cinema = new CinnamonCinema();
+        assertTrue(cinema.buyFilmTickets("fred",2));
+
     }
 }
