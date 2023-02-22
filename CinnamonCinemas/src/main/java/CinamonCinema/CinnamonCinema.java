24a803c5102ef64865e14a4c94dd2b435f2126bb
jimdeal <jimdeal@hotmail.com>
2023-02-22 13:56:29 +0000

    extend code to take two ticket purchases - keep in same row
    add in another test that will take third purchase in to next row

--- CinnamonCinemas/src/main/java/CinamonCinema/CinnamonCinema.java
+++ CinnamonCinemas/src/main/java/CinamonCinema/CinnamonCinema.java
@@ -3,7 +3,7 @@ package CinamonCinema;
 public class CinnamonCinema {
 
     private final int NUMBER_OF_ROWS = 3;
-    private final int NUMBER_OF_SEATS_IN_ROW = 56;
+    private final int NUMBER_OF_SEATS_IN_ROW = 5;
 
     private String[][] screenSeats = new String[][]{{"-1","-1","-1","-1","-1","0"},
             {"-1","-1","-1","-1","-1","0"},{"-1","-1","-1","-1","-1","0"}};
@@ -13,11 +13,13 @@ public class CinnamonCinema {
         for(int row = 0; row < NUMBER_OF_ROWS; row++){
             int takenSeatsInRow = Integer.parseInt(screenSeats[row][5]);
             if(NUMBER_OF_SEATS_IN_ROW - takenSeatsInRow - 1 >= numberOfTickets){
-                for(int fillSeats = takenSeatsInRow; fillSeats < numberOfTickets; fillSeats++){
+                int seatsToFill = takenSeatsInRow + numberOfTickets;
+                for(int fillSeats = takenSeatsInRow; fillSeats < seatsToFill; fillSeats++){
                     screenSeats[row][fillSeats] = user;
                 }
-                screenSeats[row][5] = Integer.toString(numberOfTickets);
+                screenSeats[row][5] = Integer.toString(seatsToFill);
                 purchaseSuccess = true;
+                break;
             }
         }
 
--- CinnamonCinemas/src/test/java/CinamonCinema/CinnamonCinemaTest.java
+++ CinnamonCinemas/src/test/java/CinamonCinema/CinnamonCinemaTest.java
@@ -10,6 +10,22 @@ class CinnamonCinemaTest {
     void buyFilmTickets() {
         CinnamonCinema cinema = new CinnamonCinema();
         assertTrue(cinema.buyFilmTickets("fred",2));
-
     }
+
+    @Test
+    void twoUsersBuyFilmTickets() {
+        CinnamonCinema cinema = new CinnamonCinema();
+        assertTrue(cinema.buyFilmTickets("ted",2));
+        assertTrue(cinema.buyFilmTickets("bill",2));
+    }
+
+
+    @Test
+    void threeUsersBuyFilmTickets() {
+        CinnamonCinema cinema = new CinnamonCinema();
+        assertTrue(cinema.buyFilmTickets("ted",2));
+        assertTrue(cinema.buyFilmTickets("bill",2));
+        assertTrue(cinema.buyFilmTickets("sue",2));
+    }
+
 }
