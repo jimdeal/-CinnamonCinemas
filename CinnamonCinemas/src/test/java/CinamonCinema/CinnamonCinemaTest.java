715267496e3bb62ae387f5f3fec521195be21cee
jimdeal <jimdeal@hotmail.com>
2023-02-23 10:32:19 +0000

    add in first two random test (ensure 2nd row distribution goes to 2nd row)

--- CinnamonCinemas/src/test/java/CinamonCinema/CinnamonCinemaTest.java
+++ CinnamonCinemas/src/test/java/CinamonCinema/CinnamonCinemaTest.java
@@ -62,12 +62,35 @@ class CinnamonCinemaTest {
 
     @Test
     void buyFilmTicketsRandom() {
+        CinnamonCinema cinema = new CinnamonCinema();
+        Random rand = new Random();
+        int randomSeats = rand.nextInt(4);
+        assertTrue(cinema.buyFilmTickets("fred",randomSeats));
+        assertEquals((5 - randomSeats),cinema.seatsLeftInRow(0));
     }
 
     @Test
     void twoUsersBuyFilmTicketsRandom() {
+        int min = 2; // Minimum value of range
+        int max = 3;
+
+        CinnamonCinema cinema = new CinnamonCinema();
+        Random rand = new Random();
+        int randomSeats1 = (int)Math.floor(Math.random() * (max - min + 1) + min);
+        assertTrue(cinema.buyFilmTickets("ted",randomSeats1));
+        int seatsLeft1 = 5 - randomSeats1;
+        assertEquals(seatsLeft1,cinema.seatsLeftInRow(0));
+
+        int randomSeats2 = (int)Math.floor(Math.random() * (max - min + 1) + min);
+        assertTrue(cinema.buyFilmTickets("bill",randomSeats2));
+        if((randomSeats1 + randomSeats2) <= 5){
+            int seatsLeft2 = 5 - (randomSeats1 + randomSeats2);
+            assertEquals(seatsLeft2,cinema.seatsLeftInRow(0));
+        } else {
+            int seatsLeft11 = 5 - randomSeats1;;
+            int seatsLeft12 = 5 - randomSeats2;
+            assertEquals(seatsLeft11,cinema.seatsLeftInRow(0));
+            assertEquals(seatsLeft12,cinema.seatsLeftInRow(1));
+        }
     }
-
-
-
 }
